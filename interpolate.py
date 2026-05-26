29ad0478105289417c72b5f7f36fcc4c5f856707
Alexander Kurz <alex.kurz95@gmail.com>
2026-05-26 17:08:16 +0200

    Add ImageDataset class

--- interpolate.py
+++ interpolate.py
@@ -2,8 +2,8 @@
 # Imports
 import torch
 
-from src.model import ffhq256_autoenc, build_interpolator
-
+from src.dataset import ImageDataset
+from src.model import build_interpolator, ffhq256_autoenc
 
 # %%
 # Load model
@@ -14,5 +14,18 @@ model = build_interpolator(
     device="cuda:0",
     strict=False,
 )
+
+# %%
+# Load dataset
+data = ImageDataset("data/imgs_interpolate", image_size=conf.img_size, exts=["jpg", "JPG", "png"], do_augment=False)
+batch = torch.stack(
+    [
+        data[0]["img"],
+        data[1]["img"],
+    ]
+)
+print(type(batch[0]))
+print(batch[0].shape)  # C, H, W
+ori = (batch + 1) / 2  # Undo normalization
 
 # %%
